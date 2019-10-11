6d945ebab99cc6003d66c4e8e1cf6ff6ff14f914
thangaraj.m <thangaraj.murugesan@pptservices.com>
2019-10-11 12:00:52 +0530

    changes completed

--- custom_account/models/account.py
+++ custom_account/models/account.py
@@ -2,13 +2,50 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models, fields, api, _
 
+class Company(models.Model):
+    _inherit = "res.company"
+    
+    fax = fields.Char('Fax')
+    street1 = fields.Char('Street 1')
+    
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     
+    @api.multi
+    def _get_do(self):
+#         sale_id = self.env['sale.order'].search([('name','=',self.origin)],limit=1)
+#         do_name = ''
+#         if sale_id:
+#             do_id = self.env['stock.picking'].search([('sale_id','=',sale_id.id)])
+#             for rec in do_id:
+#                 if not do_name:
+#                     do_name =  rec.name
+#                 else:
+#                     do_name += ','+ rec.name
+#         self.do_name = do_name
+#         for line in self.invoice_line_ids:  
+        sale_order_line_id = self.env['sale.order.line'].search([('invoice_lines','in',self.invoice_line_ids.ids)])
+        do_name = ''
+        if sale_order_line_id:
+            move_id = self.env['stock.move'].search([('sale_line_id','=',sale_order_line_id.ids),('state','=','done')])
+            print()
+            if move_id:
+                do_id = {}
+                for rec in move_id:
+                    do_id.setdefault(rec.picking_id,[]).append(rec)
+                for key,val in do_id.items():
+                    if not do_name:
+                        do_name =  key.name
+                    else:
+                        do_name += ','+ key.name
+                self.do_name = do_name 
+        print(do_name,'rraaraaa')  
+#         print (sale_line_id,'tttttttttttttttttttt')
     currency_conv_rate = fields.Float('Currency Conversation Rate', copy=False, readonly=True, help='It will show the currency conversation value against the invoice currency id.')
     company_currency_id = fields.Many2one('res.currency', 'Invoice Rate Currency', copy=False, related='company_id.currency_id', readonly=True)
     exchange_rate = fields.Float('Exchange Rate', digits=(12,6), copy=False, readonly=True, help='The specific rate that will be used, in this invoice, between the selected currency (in \'Invoice Rate Currency\' field)  and the Invoice currency.')
     currency_help_label = fields.Text(string="Helping Sentence", copy=False, readonly=True, help="This sentence helps you to know how to specify the invoice rate by giving you the direct effect it has")
+    do_name = fields.Char(string="DO No.",compute='_get_do',readonly=True)
      
     @api.model
     def create(self, vals):
@@ -20,19 +57,19 @@ class AccountInvoice(models.Model):
                     res.exchange_rate = rate_id.rate
                     res.currency_conv_rate = (1/rate_id.rate)
                     val = round(res.currency_conv_rate,2)
-                    res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)
+                    res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)+'\n'+res.company_currency_id.symbol+'1.00 = '+res.currency_id.symbol+' '+str(res.exchange_rate)   
                 else:
                     rate_id = self.env['res.currency'].search([('id','=',res.currency_id.id)])
                     res.exchange_rate = rate_id.rate
                     res.currency_conv_rate = (1/rate_id.rate)
                     val = round(res.currency_conv_rate,2)
-                    res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)
+                    res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)+'\n'+res.company_currency_id.symbol+'1.00 = '+res.currency_id.symbol+' '+str(res.exchange_rate)
             else:
                 rate_id = self.env['res.currency'].search([('id','=',res.currency_id.id)])
                 res.exchange_rate = rate_id.rate
                 res.currency_conv_rate = (1/rate_id.rate)
                 val = round(res.currency_conv_rate,2)
-                res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)
+                res.currency_help_label = 'At the operation date, the exchange rate was \n'+res.currency_id.symbol+'1.00 = '+res.company_id.currency_id.symbol+' '+str(val)+'\n'+res.company_currency_id.symbol+'1.00 = '+res.currency_id.symbol+' '+str(res.exchange_rate)
         else:
             res.exchange_rate = 0.00
             res.currency_conv_rate = 0.00
@@ -48,24 +85,24 @@ class AccountInvoice(models.Model):
                     self.exchange_rate = rate_id.rate
                     self.currency_conv_rate = (1/rate_id.rate)
                     val = round(self.currency_conv_rate,2)
-                    self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)
+                    self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)+'\n'+self.company_currency_id.symbol+'1.00 = '+self.currency_id.symbol+' '+str(self.exchange_rate)
                 else:
                     rate_id = self.env['res.currency'].search([('id','=',self.currency_id.id)])
                     self.exchange_rate = rate_id.rate
                     self.currency_conv_rate = (1/rate_id.rate)
                     val = round(self.currency_conv_rate,2)
-                    self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)
+                    self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)+'\n'+self.company_currency_id.symbol+'1.00 = '+self.currency_id.symbol+' '+str(self.exchange_rate)
             else:
                 rate_id = self.env['res.currency'].search([('id','=',self.currency_id.id)])
                 self.exchange_rate = rate_id.rate
                 self.currency_conv_rate = (1/rate_id.rate)
                 val = round(self.currency_conv_rate,2)
-                self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)
+                self.currency_help_label = 'At the operation date, the exchange rate was \n'+self.currency_id.symbol+'1.00 = '+self.company_id.currency_id.symbol+' '+str(val)+'\n'+self.company_currency_id.symbol+'1.00 = '+self.currency_id.symbol+' '+str(self.exchange_rate)
         else:
             self.exchange_rate = 0.00
             self.currency_conv_rate = 0.00
             self.currency_help_label = ''
-
+    
     def get_currency_conv_rate(self,):
         conv_rate = 0.00
         for record in self:
